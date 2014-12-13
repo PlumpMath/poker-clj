b38dabe6aa84f09141af1e493a77dd39aac66d41
Ryan <ryan.joshua.jones@gmail.com>
2014-12-12 16:52:45 -0800

    let's do this

---
+++ src/poker/core.clj
@@ -0,0 +1,27 @@
+(ns poker.core
+  (:require [clojure.string :as str]))
+
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s)))
+
+(defn parse-rank
+  [rank]
+  (case rank
+    "T" 10
+    "J" 11
+    "Q" 12
+    "K" 13
+    "A" 14
+    (parse-int rank)))
+
+(defn parse-card
+  "parses a string like \"5S\" and returns a map with :suit and :rank"
+  [card]
+  (let [[_ val suit] (str/split card #"")
+        rank (parse-rank val)]
+    {:suit suit
+     :rank rank}))
+
+(def my-hand (map parse-card ["5S" "TH" "5S" "4H" "3C"]))
+(frequencies (map :rank my-hand))
+
